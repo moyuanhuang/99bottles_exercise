e4fb00e3fbfaf8450f4a41aded0e25e568dcfc13
moyuanhuang <moyuanhuang@gmail.com>
2018-06-03 11:19:12 -0700

    Pass test_the_first_verse and test_another_verse

---
+++ lib/bottles.rb
@@ -0,0 +1,9 @@
+class Bottles
+  def initializer
+  end
+
+  def verse(n)
+    "#{n} bottles of beer on the wall, #{n} bottles of beer.\n" \
+    "Take one down and pass it around, #{n - 1} bottles of beer on the wall.\n"
+  end
+end
